d0dc00e6d1efc06ca84a40bf000fbd3f3f032603
BigAbe97 <abraham13wilson@gmail.com>
2023-03-24 15:28:24 +0000

    Combine workbooks
    Combined the workbooks into one and used write_csv() to write into processed folder. Also read the file from the processed folder and displayed the dataframe in the R markdown.

---
+++ scripts/render_data_cleaning_html.R
@@ -0,0 +1,9 @@
+# The code below renders the data_cleaning.Rmd file available in the notebooks folder to an HTML file in the reports folder.
+
+library(rmarkdown)
+code_dir <- "notebooks"
+report_filename <- "data_cleaning.Rmd"
+report_filename <- file.path(code_dir, report_filename)
+output_dir <- "reports"
+output <- file.path("..", output_dir)
+render(report_filename, output_dir = output_dir, params = list(output_dir = output))
